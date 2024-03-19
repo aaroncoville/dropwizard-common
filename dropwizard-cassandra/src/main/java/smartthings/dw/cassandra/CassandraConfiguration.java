e2a700c48b147275dd9b7665737afcf162570e51
Aaron Coville <aaron.coville@smartthings.com>
2024-03-19 12:13:23 -0400

    Add v3 cassandra ReqPerConn configuration

--- dropwizard-cassandra/src/main/java/smartthings/dw/cassandra/CassandraConfiguration.java
+++ dropwizard-cassandra/src/main/java/smartthings/dw/cassandra/CassandraConfiguration.java
@@ -21,154 +21,154 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class CassandraConfiguration {
-	private final static Logger LOG = LoggerFactory.getLogger(CassandraConfiguration.class);
+    private final static Logger LOG = LoggerFactory.getLogger(CassandraConfiguration.class);
 
-	private String[] cipherSuites = new String[]{"TLS_RSA_WITH_AES_128_CBC_SHA", "TLS_RSA_WITH_AES_256_CBC_SHA"};
+    private String[] cipherSuites = new String[]{"TLS_RSA_WITH_AES_128_CBC_SHA", "TLS_RSA_WITH_AES_256_CBC_SHA"};
 
-	private JKSConfig truststore;
-	private JKSConfig keystore;
+    private JKSConfig truststore;
+    private JKSConfig keystore;
 
-	private String user;
-	private String password;
+    private String user;
+    private String password;
 
-	@NotEmpty
-	private String keyspace;
+    @NotEmpty
+    private String keyspace;
 
-	private String migrationFile = "/migrations/cql.changelog";
-	private Boolean autoMigrate = false;
+    private String migrationFile = "/migrations/cql.changelog";
+    private Boolean autoMigrate = false;
 
-	private QueryOptions queryOptions;
+    private QueryOptions queryOptions;
     private PoolConfiguration pooling;
 
-	@Valid
-	private RetryPolicyFactory retryPolicy;
+    @Valid
+    private RetryPolicyFactory retryPolicy;
 
-	@Valid
-	private SpeculativeExecutionPolicyFactory speculativeExecutionPolicy;
+    @Valid
+    private SpeculativeExecutionPolicyFactory speculativeExecutionPolicy;
 
-	@Valid
-	private LoadBalancingPolicyFactory loadBalancingPolicy;
+    @Valid
+    private LoadBalancingPolicyFactory loadBalancingPolicy;
 
-	@Valid
-	private AddressTranslatorFactory addressTranslatorFactory;
+    @Valid
+    private AddressTranslatorFactory addressTranslatorFactory;
 
-	private List<String> seeds;
+    private List<String> seeds;
 
-	private int protocolVersion = 0;
+    private int protocolVersion = 0;
 
     private Long shutdownTimeoutInMillis = TimeUnit.SECONDS.toMillis(30);
 
     private String testQuery = "SELECT cql_version FROM system.local LIMIT 1";
 
-	public JKSConfig getTruststore() {
-		return truststore;
-	}
+    public JKSConfig getTruststore() {
+        return truststore;
+    }
 
-	public void setTruststore(JKSConfig truststore) {
-		this.truststore = truststore;
-	}
+    public void setTruststore(JKSConfig truststore) {
+        this.truststore = truststore;
+    }
 
-	public JKSConfig getKeystore() {
-		return keystore;
-	}
+    public JKSConfig getKeystore() {
+        return keystore;
+    }
 
-	public void setKeystore(JKSConfig keystore) {
-		this.keystore = keystore;
-	}
+    public void setKeystore(JKSConfig keystore) {
+        this.keystore = keystore;
+    }
 
-	public String getUser() {
-		return user;
-	}
+    public String getUser() {
+        return user;
+    }
 
-	public void setUser(String user) {
-		this.user = user;
-	}
+    public void setUser(String user) {
+        this.user = user;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public String getKeyspace() {
-		return keyspace;
-	}
+    public String getKeyspace() {
+        return keyspace;
+    }
 
-	public void setKeyspace(String keyspace) {
-		this.keyspace = keyspace;
-	}
+    public void setKeyspace(String keyspace) {
+        this.keyspace = keyspace;
+    }
 
-	public List<String> getSeeds() {
-		return seeds;
-	}
+    public List<String> getSeeds() {
+        return seeds;
+    }
 
-	public void setSeeds(List<String> seeds) {
-		this.seeds = seeds;
-	}
+    public void setSeeds(List<String> seeds) {
+        this.seeds = seeds;
+    }
 
-	public RegularStatement getValidationQuery() {
-	    SimpleStatement statement = new  SimpleStatement(testQuery);
-	    statement.setIdempotent(true);
-	    return statement;
-	}
+    public RegularStatement getValidationQuery() {
+        SimpleStatement statement = new SimpleStatement(testQuery);
+        statement.setIdempotent(true);
+        return statement;
+    }
 
-	public String getMigrationFile() {
-		return migrationFile;
-	}
+    public String getMigrationFile() {
+        return migrationFile;
+    }
 
-	public void setMigrationFile(String migrationFile) {
-		this.migrationFile = migrationFile;
-	}
+    public void setMigrationFile(String migrationFile) {
+        this.migrationFile = migrationFile;
+    }
 
-	public Boolean getAutoMigrate() {
-		return autoMigrate;
-	}
+    public Boolean getAutoMigrate() {
+        return autoMigrate;
+    }
 
-	public void setAutoMigrate(Boolean autoMigrate) {
-		this.autoMigrate = autoMigrate;
-	}
+    public void setAutoMigrate(Boolean autoMigrate) {
+        this.autoMigrate = autoMigrate;
+    }
 
-	public QueryOptions getQueryOptions() {
-		return queryOptions;
-	}
+    public QueryOptions getQueryOptions() {
+        return queryOptions;
+    }
 
-	public void setQueryOptions(QueryOptions queryOptions) {
-		this.queryOptions = queryOptions;
-	}
+    public void setQueryOptions(QueryOptions queryOptions) {
+        this.queryOptions = queryOptions;
+    }
 
-	public RetryPolicyFactory getRetryPolicy() {
-		return retryPolicy;
-	}
+    public RetryPolicyFactory getRetryPolicy() {
+        return retryPolicy;
+    }
 
-	public void setRetryPolicy(RetryPolicyFactory retryPolicy) {
-		this.retryPolicy = retryPolicy;
-	}
+    public void setRetryPolicy(RetryPolicyFactory retryPolicy) {
+        this.retryPolicy = retryPolicy;
+    }
 
-	public SpeculativeExecutionPolicyFactory getSpeculativeExecutionPolicy() {
-		return speculativeExecutionPolicy;
-	}
+    public SpeculativeExecutionPolicyFactory getSpeculativeExecutionPolicy() {
+        return speculativeExecutionPolicy;
+    }
 
-	public void setSpeculativeExecutionPolicy(SpeculativeExecutionPolicyFactory speculativeExecutionPolicy) {
-		this.speculativeExecutionPolicy = speculativeExecutionPolicy;
-	}
+    public void setSpeculativeExecutionPolicy(SpeculativeExecutionPolicyFactory speculativeExecutionPolicy) {
+        this.speculativeExecutionPolicy = speculativeExecutionPolicy;
+    }
 
-	public LoadBalancingPolicyFactory getLoadBalancingPolicy() {
-		return loadBalancingPolicy;
-	}
+    public LoadBalancingPolicyFactory getLoadBalancingPolicy() {
+        return loadBalancingPolicy;
+    }
 
-	public void setLoadBalancingPolicy(LoadBalancingPolicyFactory loadBalancingPolicy) {
-		this.loadBalancingPolicy = loadBalancingPolicy;
-	}
+    public void setLoadBalancingPolicy(LoadBalancingPolicyFactory loadBalancingPolicy) {
+        this.loadBalancingPolicy = loadBalancingPolicy;
+    }
 
-	public AddressTranslatorFactory getAddressTranslatorFactory() {
-		return addressTranslatorFactory;
-	}
+    public AddressTranslatorFactory getAddressTranslatorFactory() {
+        return addressTranslatorFactory;
+    }
 
-	public void setAddressTranslatorFactory(AddressTranslatorFactory addressTranslatorFactory) {
-		this.addressTranslatorFactory = addressTranslatorFactory;
-	}
+    public void setAddressTranslatorFactory(AddressTranslatorFactory addressTranslatorFactory) {
+        this.addressTranslatorFactory = addressTranslatorFactory;
+    }
 
     public Long getShutdownTimeoutInMillis() {
         return shutdownTimeoutInMillis;
@@ -187,11 +187,11 @@ public class CassandraConfiguration {
     }
 
     public int getProtocolVersion() {
-	    return this.protocolVersion;
+        return this.protocolVersion;
     }
 
     public void setProtocolVersion(int protocolVersion) {
-	    this.protocolVersion = protocolVersion;
+        this.protocolVersion = protocolVersion;
     }
 
     public PoolConfiguration getPooling() {
@@ -204,125 +204,125 @@ public class CassandraConfiguration {
 
     public static class JKSConfig {
 
-		String path;
-		String password;
+        String path;
+        String password;
 
-		public JKSConfig() {
-		}
+        public JKSConfig() {
+        }
 
-		public String getPath() {
-			return path;
-		}
+        public String getPath() {
+            return path;
+        }
 
-		public void setPath(String path) {
-			this.path = path;
-		}
+        public void setPath(String path) {
+            this.path = path;
+        }
 
-		public String getPassword() {
-			return password;
-		}
+        public String getPassword() {
+            return password;
+        }
 
-		public void setPassword(String password) {
-			this.password = password;
-		}
-	}
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
 
-	public Session buildSession() {
-		Session session;
-		Cluster cluster;
+    public Session buildSession() {
+        Session session;
+        Cluster cluster;
 
-		Cluster.Builder builder = Cluster.builder();
+        Cluster.Builder builder = Cluster.builder();
 
-		if (loadBalancingPolicy != null) {
-			builder.withLoadBalancingPolicy(loadBalancingPolicy.build());
-		}
+        if (loadBalancingPolicy != null) {
+            builder.withLoadBalancingPolicy(loadBalancingPolicy.build());
+        }
 
-		if (speculativeExecutionPolicy != null) {
-			builder.withSpeculativeExecutionPolicy(speculativeExecutionPolicy.build());
-		}
+        if (speculativeExecutionPolicy != null) {
+            builder.withSpeculativeExecutionPolicy(speculativeExecutionPolicy.build());
+        }
 
-		if (addressTranslatorFactory != null) {
-			builder.withAddressTranslator(addressTranslatorFactory.build());
-		}
+        if (addressTranslatorFactory != null) {
+            builder.withAddressTranslator(addressTranslatorFactory.build());
+        }
 
-		if (queryOptions != null) {
-			LOG.info("QueryOptions - ConsistencyLevel {}", queryOptions.getConsistencyLevel());
-			LOG.info("QueryOptions - SerialConsistencyLevel {}", queryOptions.getSerialConsistencyLevel());
-			LOG.info("QueryOptions - FetchSize {}", queryOptions.getFetchSize());
-			LOG.info("QueryOptions - DefaultIdempotence {}", queryOptions.getDefaultIdempotence());
-			builder.withQueryOptions(queryOptions);
-		}
+        if (queryOptions != null) {
+            LOG.info("QueryOptions - ConsistencyLevel {}", queryOptions.getConsistencyLevel());
+            LOG.info("QueryOptions - SerialConsistencyLevel {}", queryOptions.getSerialConsistencyLevel());
+            LOG.info("QueryOptions - FetchSize {}", queryOptions.getFetchSize());
+            LOG.info("QueryOptions - DefaultIdempotence {}", queryOptions.getDefaultIdempotence());
+            builder.withQueryOptions(queryOptions);
+        }
 
         if (pooling != null) {
             LOG.info("PoolOptions - {}", pooling);
             builder.withPoolingOptions(pooling.toPoolOptions());
         }
 
-		if (retryPolicy != null) {
-			builder.withRetryPolicy(retryPolicy.build());
-		}
+        if (retryPolicy != null) {
+            builder.withRetryPolicy(retryPolicy.build());
+        }
 
-		for (String seed : seeds) {
-			if (seed.contains(":")) {
-				String[] tokens = seed.split(":");
-				LOG.info("Seed - {}:{}", tokens[0], tokens[1]);
-				builder.addContactPoint(tokens[0]).withPort(Integer.parseInt(tokens[1]));
-			} else {
-				LOG.info("Seed - {}", seed);
-				builder.addContactPoint(seed);
-			}
-		}
+        for (String seed : seeds) {
+            if (seed.contains(":")) {
+                String[] tokens = seed.split(":");
+                LOG.info("Seed - {}:{}", tokens[0], tokens[1]);
+                builder.addContactPoint(tokens[0]).withPort(Integer.parseInt(tokens[1]));
+            } else {
+                LOG.info("Seed - {}", seed);
+                builder.addContactPoint(seed);
+            }
+        }
 
-		if (truststore != null && ! Strings.isNullOrEmpty(truststore.getPath())) {
-			try {
-				LOG.info("With SSL");
-				SSLContext sslContext = getSSLContext(truststore.path, truststore.password, keystore.path, keystore.password);
-				builder.withSSL(new JdkSSLOptions.Builder()
+        if (truststore != null && !Strings.isNullOrEmpty(truststore.getPath())) {
+            try {
+                LOG.info("With SSL");
+                SSLContext sslContext = getSSLContext(truststore.path, truststore.password, keystore.path, keystore.password);
+                builder.withSSL(new JdkSSLOptions.Builder()
                     .withSSLContext(sslContext)
                     .withCipherSuites(cipherSuites)
                     .build());
-			} catch (Exception e) {
-				LOG.error("Couldn't add SSL to the cluster builder.", e);
-			}
-		}
-
-		if (! Strings.isNullOrEmpty(user) && ! Strings.isNullOrEmpty(password)) {
-			LOG.info("Credentials - {}", user);
-			builder.withCredentials(user, password);
-		}
-
-		if (protocolVersion != 0) {
-		    builder.withProtocolVersion(ProtocolVersion.fromInt(protocolVersion));
-		    LOG.info("Protocol Version - {}", protocolVersion);
+            } catch (Exception e) {
+                LOG.error("Couldn't add SSL to the cluster builder.", e);
+            }
         }
 
-		cluster = builder.build();
+        if (!Strings.isNullOrEmpty(user) && !Strings.isNullOrEmpty(password)) {
+            LOG.info("Credentials - {}", user);
+            builder.withCredentials(user, password);
+        }
 
-		LOG.info("Keyspace - {}", keyspace);
-		session = cluster.connect(keyspace);
-		LOG.info("Connection Created - {}", session);
-		return session;
-	}
+        if (protocolVersion != 0) {
+            builder.withProtocolVersion(ProtocolVersion.fromInt(protocolVersion));
+            LOG.info("Protocol Version - {}", protocolVersion);
+        }
 
-	private static SSLContext getSSLContext(String truststorePath, String truststorePassword, String keystorePath, String keystorePassword) throws Exception {
-		FileInputStream tsf = new FileInputStream(truststorePath);
-		FileInputStream ksf = new FileInputStream(keystorePath);
-		SSLContext ctx = SSLContext.getInstance("SSL");
+        cluster = builder.build();
 
-		KeyStore ts = KeyStore.getInstance("JKS");
-		ts.load(tsf, truststorePassword.toCharArray());
-		TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-		tmf.init(ts);
+        LOG.info("Keyspace - {}", keyspace);
+        session = cluster.connect(keyspace);
+        LOG.info("Connection Created - {}", session);
+        return session;
+    }
 
-		KeyStore ks = KeyStore.getInstance("JKS");
-		ks.load(ksf, keystorePassword.toCharArray());
-		KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+    private static SSLContext getSSLContext(String truststorePath, String truststorePassword, String keystorePath, String keystorePassword) throws Exception {
+        FileInputStream tsf = new FileInputStream(truststorePath);
+        FileInputStream ksf = new FileInputStream(keystorePath);
+        SSLContext ctx = SSLContext.getInstance("SSL");
 
-		kmf.init(ks, keystorePassword.toCharArray());
+        KeyStore ts = KeyStore.getInstance("JKS");
+        ts.load(tsf, truststorePassword.toCharArray());
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        tmf.init(ts);
 
-		ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new SecureRandom());
-		return ctx;
-	}
+        KeyStore ks = KeyStore.getInstance("JKS");
+        ks.load(ksf, keystorePassword.toCharArray());
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+
+        kmf.init(ks, keystorePassword.toCharArray());
+
+        ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new SecureRandom());
+        return ctx;
+    }
 
     public static class PoolConfiguration {
         private Integer idleTimeoutSeconds;
@@ -331,6 +331,8 @@ public class CassandraConfiguration {
         private Integer poolTimeoutMillis;
         private Integer localMaxConnPerHost;
         private Integer remoteMaxConnPerHost;
+        private Integer localMaxRequestsPerConn;
+        private Integer remoteMaxRequestsPerConn;
 
         public PoolConfiguration() {
 
@@ -384,6 +386,22 @@ public class CassandraConfiguration {
             this.remoteMaxConnPerHost = remoteMaxConnPerHost;
         }
 
+        public Integer getRemoteMaxRequestsPerConn() {
+            return remoteMaxRequestsPerConn;
+        }
+
+        public void setRemoteMaxRequestsPerConn(Integer remoteMaxRequestsPerConn) {
+            this.remoteMaxRequestsPerConn = remoteMaxRequestsPerConn;
+        }
+
+        public Integer getLocalMaxRequestsPerConn() {
+            return localMaxRequestsPerConn;
+        }
+
+        public void setLocalMaxRequestsPerConn(Integer localMaxRequestsPerConn) {
+            this.localMaxRequestsPerConn = localMaxRequestsPerConn;
+        }
+
         @Override
         public String toString() {
             return "PoolConfiguration{" +
@@ -393,6 +411,8 @@ public class CassandraConfiguration {
                 ", poolTimeoutMillis=" + poolTimeoutMillis +
                 ", localMaxConnPerHost=" + localMaxConnPerHost +
                 ", remoteMaxConnPerHost=" + remoteMaxConnPerHost +
+                ", localMaxRequestsPerConn=" + localMaxRequestsPerConn +
+                ", remoteMaxRequestsPerConn=" + remoteMaxRequestsPerConn +
                 '}';
         }
 
@@ -422,6 +442,15 @@ public class CassandraConfiguration {
             if (remoteMaxConnPerHost != null) {
                 options.setMaxConnectionsPerHost(HostDistance.REMOTE, remoteMaxConnPerHost);
             }
+
+            if (localMaxRequestsPerConn != null) {
+                options.setMaxRequestsPerConnection(HostDistance.LOCAL, localMaxRequestsPerConn);
+            }
+
+            if (remoteMaxRequestsPerConn != null) {
+                options.setMaxRequestsPerConnection(HostDistance.REMOTE, remoteMaxRequestsPerConn);
+            }
+
             return options;
         }
     }
